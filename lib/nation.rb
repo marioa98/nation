ef9aa77f9fc853a900a5af6be3370233820932f3
Mario Martinez <mmartinez53@ucol.mx>
2020-02-10 19:48:44 -0600

    Partial refactor of Nation.rb and the rest of the documents commented

--- lib/nation.rb
+++ lib/nation.rb
@@ -1,33 +1,56 @@
+require 'net/http'
+require 'json'
+
 class Nation
-
-  def initialize
-    @stored_info = {}
+  def initialize(nation_name)
+    @nation_info = {}
+    @nation_code = standarize(nation_name)
   end
 
-  def nation_info(nation_name)
-    nation_name = nation_name.downcase.split.map(&:capitalize).join("_")
-    general = NationGeneral.new(nation_name)
-    nation_general = general.get_raw_data 
+  def standarize(full_name)
+    uri = URI.parse("http://country.io/names.json")
+    response = Net::HTTP.get(uri)
 
-    info = NationInfo.new(nation_name)
-    info_country = info.get_raw_data
-
-     @stored_info = {:name => nation_general[0], :capital => nation_general[1], :continent => nation_general[2], :region => nation_general[3], :population => nation_general[4], :languages => nation_general[5], :about_country => info_country}
-
-     @stored_info
-
+    nation_code = JSON.parse(response).select{|k, v| v == full_name}.keys[0]
   end
 
-  def info_to_json
-    JsonFile.convert_json_file(@stored_info)    
+  def get_info
   end
 
-  def info_to_html
-    HtmlFile.convert_html_file(@stored_info)
+  def print_file(ext_file)
   end
 end
 
-require './nation/nation_general'
-require './nation/nation_info'
-require './nation/templates/json_file'
-require './nation/templates/html_file'
+nation = Nation.new("United Kingdom")
+puts nation.nation_code
+# require './nation/nation_general'
+# require './nation/nation_info'
+# require './nation/templates/json_file'
+# require './nation/templates/html_file'
+
+# class Nation
+
+#   def initialize
+#     @stored_info = {}
+#   end
+
+#   def nation_info(nation_name)
+#     nation_name = nation_name.downcase.split.map(&:capitalize).join("_")
+#     general = NationGeneral.new(nation_name)
+#     nation_general = general.get_raw_data 
+
+#     info = NationInfo.new(nation_name)
+#     info_country = info.get_raw_data
+
+#      @stored_info = {:name => nation_general[0], :capital => nation_general[1], :continent => nation_general[2], :region => nation_general[3], :population => nation_general[4], :languages => nation_general[5], :about_country => info_country}
+
+#   end
+
+#   def info_to_json
+#     JsonFile.convert_json_file(@stored_info)    
+#   end
+
+#   def info_to_html
+#     HtmlFile.convert_html_file(@stored_info)
+#   end
+# end
